b6aa4a397bfe95403d8c71832ff718101c616683
Anay Wadhera <awadhera@berkeley.edu>
2020-10-02 16:26:35 -0700

    vendor: allow customizing the length of gesture navbar [3/3]
    Change-Id: I9fc95f315d5cf7a2b9a90af9fdce9eadce518653
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/3139
    Tested-by: Andrew Thomas <itz63c@statixos.com>
    Reviewed-by: Andrew Thomas <itz63c@statixos.com>

--- build/product.mk
+++ build/product.mk
@@ -121,3 +121,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.kb_pad_port_r=4 \
     ro.com.google.ime.kb_pad_land_l=64 \
     ro.com.google.ime.kb_pad_land_r=64 \
+
+# Overlays
+PRODUCT_PACKAGES += \
+    GesturalNavigationOverlayLong \
+    GesturalNavigationOverlayMedium
