0ba66ba7c303044f0ae021ac1e8e8ff1788b8e24
Nauval Rizky <enuma.alrizky@gmail.com>
2022-04-24 03:11:53 +0000

    apps: Conditionally build and use GameSpace
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- apps/apps.mk
+++ apps/apps.mk
@@ -37,6 +37,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     RepainterServicePriv \
 
+# Gamespace
+ifneq (,$(wildcard packages/apps/GameSpace))
+PRODUCT_PACKAGES += \
+    GameSpace
+endif
+
 # Permissions for Google product apps
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/default-permissions-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permissions-product.xml \
